168538598266b96c8c15e91638191ba1eb219b10
Daniel Erenrich <erenrich@svm>
2012-11-28 00:26:59 -0500

    add working logistic regression classifier

---
+++ basic_logistic.py
@@ -0,0 +1,104 @@
+import os
+import sys
+import time
+import numpy
+import theano
+import theano.tensor as T
+from util import *
+
+
+class LogisticRegression(object):
+    def __init__(self, input, n_in, n_out):
+        # initialize with 0 the weights W as a matrix of shape (n_in, n_out)
+        self.W = theano.shared(value=numpy.zeros((n_in, n_out),
+                                                 dtype=theano.config.floatX),
+                                name='W', borrow=True)
+        # initialize the baises b as a vector of n_out 0s
+        self.b = theano.shared(value=numpy.zeros((n_out,),
+                                                 dtype=theano.config.floatX),
+                               name='b', borrow=True)
+
+        # compute vector of class-membership probabilities in symbolic form
+        self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
+
+        # compute prediction as class whose probability is maximal in
+        # symbolic form
+        self.y_pred = T.argmax(self.p_y_given_x, axis=1)
+
+        # parameters of the model
+        self.params = [self.W, self.b]
+
+    def negative_log_likelihood(self, y):
+        return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
+
+    def errors(self, y):
+        # check if y has same dimension of y_pred
+        if y.ndim != self.y_pred.ndim:
+            raise TypeError('y should have the same shape as self.y_pred',
+                ('y', target.type, 'y_pred', self.y_pred.type))
+        # check if y is of the correct datatype
+        if y.dtype.startswith('int'):
+            # the T.neq operator returns a vector of 0s and 1s, where 1
+            # represents a mistake in prediction
+            return T.mean(T.neq(self.y_pred, y))
+        else:
+            raise NotImplementedError()
+
+
+
+def learn(batch_size = 50,
+          learning_rate=0.13,
+          n_epochs=10000):
+    data = load_data(validation_sample = 0)
+    train_x, train_y = data[0]
+    test_x, test_y = data[1]
+    
+    n_train_batches = train_x.get_value(borrow=True).shape[0] / batch_size
+    print "Building Model"
+
+    x = T.matrix('x')
+    y = T.ivector('y')
+    image_size = train_x.get_value(borrow=True).shape[1]
+    classifier = LogisticRegression(input = x,
+                                    n_in = image_size,
+                                    n_out = 3)
+    cost = classifier.negative_log_likelihood(y)
+    g_W = T.grad(cost=cost, wrt=classifier.W)
+    g_b = T.grad(cost=cost, wrt=classifier.b)
+    updates = {classifier.W: classifier.W - learning_rate * g_W,
+               classifier.b: classifier.b - learning_rate * g_b}
+
+
+    test_model = theano.function(inputs=[],
+            outputs=classifier.errors(y),
+            givens={
+                x: test_x,
+                y: test_y})
+    test_train_model = theano.function(inputs=[],
+            outputs=classifier.errors(y),
+            givens={
+                x: train_x,
+                y: train_y})
+    index = T.lscalar()
+    train_model = theano.function(inputs=[index],
+                                  outputs=cost,
+                                  updates=updates,
+                                  givens={
+                                      x: train_x[index * batch_size:(index + 1) * batch_size],
+                                      y: train_y[index * batch_size:(index + 1) * batch_size]}, name="train_model")
+
+    print "Training Model"
+    epoch = 0
+    updates = 0
+    total_cost = 0.0
+    while epoch < n_epochs:
+        epoch += 1
+        for minibatch_idx in range(n_train_batches):
+            batch_cost = train_model(minibatch_idx)
+            total_cost += batch_cost
+            updates += 1
+            if updates % 1000 == 0:
+                print "Test Errors", test_model()
+                print "Train Errors", test_train_model()
+if __name__ == '__main__':
+    learn()
--- util.py
+++ util.py
@@ -16,7 +16,15 @@ def shared_dataset(data_xy, borrow=True):
                              borrow=borrow)
     return shared_x, T.cast(shared_y, 'int32')
 
-def load_data(sample = 1.0):
+def shuffle(x,y):
+    rs = map(lambda i : random.random(), x)
+    x = map(lambda v : v[1] , sorted(zip(rs,x)))
+    y = map(lambda v : v[1] , sorted(zip(rs,y)))
+    return (x,y)
+
+def load_data(sub_sample = 1.0,
+              test_sample = 0.1,
+              validation_sample=0.05):
     """
     loads our dataset into shared variables
     """
@@ -29,11 +37,21 @@ def load_data(sample = 1.0):
         paths = [('imagecl/train/bicycle/', 0),
                  ('imagecl/train/car/', 1),
                  ('imagecl/train/motorbike/', 2)]
-        images = []
-        classes = []
+        train_images = []
+        train_classes = []
+
+        test_images = []
+        test_classes = []
+
+        validate_images = []
+        validate_classes = []
+
+        images = [train_images, test_images, validate_images]
+        classes = [train_classes, test_classes, validate_classes]
         for p,c in paths:
             for im in os.listdir(p):
-                if random.random() > sample:
+                # look at this guy at all?
+                if random.random() > sub_sample:
                     next
                 i = Image.open(p + im)
                 # convert to grayscale
@@ -42,14 +60,26 @@ def load_data(sample = 1.0):
                 # convert to numpy array and normalize to [0,1]
                 i = numpy.array(i, dtype=numpy.float32)/255
                 i = i.flatten()
-                images.append(i)
-                classes.append(c)
-        classes = numpy.array(classes)
-        images = numpy.dstack(images)
-        images = numpy.squeeze(images)
-        data = (images,classes)
+                r = random.random()
+                if r < test_sample:
+                    test_images.append(i)
+                    test_classes.append(c)
+                elif r < (test_sample + validation_sample):
+                    validate_images.append(i)
+                    validate_classes.append(c)
+                else:
+                    train_images.append(i)
+                    train_classes.append(c)
+        for idx in range(len(images)):
+            (images[idx], classes[idx]) = shuffle(images[idx],classes[idx])
+        for idx in range(len(images)):
+            images[idx] = numpy.dstack(images[idx])
+            images[idx] = numpy.squeeze(images[idx])        
+            images[idx] = numpy.transpose(images[idx])   
+        for idx in range(len(classes)):
+            classes[idx] = numpy.array(classes[idx])        
+        data = zip(images,classes)
         cPickle.dump(data, open(cacheFile,'w'))
-    xs, ys = shared_dataset(data)
-    rval = (xs,ys)
+    rval = map(shared_dataset, data)
     return rval
 
