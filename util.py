7ae654fefd61eee4ecf45b61ed085105525d5db4
Daniel Rosenberg <drosenbe@Bufonidae.(none)>
2012-12-03 18:50:45 -0800

    Added a load util that chunks.

--- util.py
+++ util.py
@@ -83,3 +83,76 @@ def load_data(sub_sample = 1.0,
     rval = map(shared_dataset, data)
     return rval
 
+def load_data_chunk(sub_sample = 1.0,
+              test_sample = 0.1,
+              validation_sample=0.05,
+              chunk_size = 5,new_size=50):
+    """
+    loads our dataset into shared variables
+    """
+    data = None
+    cacheFile = 'dataset2.pkl'
+    if(os.path.isfile(cacheFile)):
+        cF = open(cacheFile)
+        data = cPickle.load(cF)
+    else:
+        paths = [('imagecl/train/bicycle/', 0),
+                 ('imagecl/train/car/', 1),
+                 ('imagecl/train/motorbike/', 2)]
+        train_images = []
+        train_classes = []
+
+        test_images = []
+        test_classes = []
+
+        validate_images = []
+        validate_classes = []
+
+        images = [train_images, test_images, validate_images]
+        classes = [train_classes, test_classes, validate_classes]
+        for p,c in paths:
+            for im in os.listdir(p):
+                # look at this guy at all?
+                if random.random() > sub_sample:
+                    next
+                i = Image.open(p + im)
+                # convert to grayscale
+                i = ImageOps.grayscale(i)
+                # rescale, but avoid most skew
+                w = i.size[0]
+                h = i.size[1]
+                i = i.resize((new_size,(h*new_size)/w),Image.ANTIALIAS)
+                # convert to numpy array and normalize to [0,1]
+                i = numpy.array(i, dtype=numpy.float32)/255
+                r = random.random()
+                if r < test_sample:
+                    i = i.flatten()
+                    test_images.append(i)
+                    test_classes.append(c)
+                elif r < (test_sample + validation_sample):
+                    i = i.flatten()
+                    validate_images.append(i)
+                    validate_classes.append(c)
+                else:
+                    # training images are special. Break into chunks
+                    for x in xrange(len(i)/chunk_size):
+                        # pull relevant strip and transpose for indexing fun.
+                        x_strip = i[x*chunk_size:(x+1)*chunk_size].transpose()
+                        for y in xrange(len(i[0])/chunk_size):
+                            Y=y*chunk_size
+                            sub_image = x_strip[y*chunk_size:(y+1)*chunk_size].transpose()
+                            train_images.append(sub_image.flatten())
+                            train_classes.append(c)
+        for idx in range(len(images)):
+            (images[idx], classes[idx]) = shuffle(images[idx],classes[idx])
+        for idx in range(len(images)):
+            images[idx] = numpy.dstack(images[idx])
+            images[idx] = numpy.squeeze(images[idx])
+            images[idx] = numpy.transpose(images[idx])
+        for idx in range(len(classes)):
+            classes[idx] = numpy.array(classes[idx])
+        data = zip(images,classes)
+        cPickle.dump(data, open(cacheFile,'w'))
+    rval = map(shared_dataset, data)
+    return rval
+
